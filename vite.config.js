becf319f362dae8dab4c713a2de1480d42838ff0
chigham <chigham@byui.edu>
2023-05-22 14:38:29 -0600

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,12 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src/cart/index.html"),
-        checkout: resolve(__dirname, "src/checkout/index.html"),
-        checkedout: resolve(__dirname, "src/checkout/success.html"),
-        product_pages: resolve(__dirname, "src/product_pages/index.html"),
-        product_listing: resolve(__dirname, "src/product_listing/index.html")
+        main: resolve(__dirname, "wdd330-w2-bonus/src/index.html"),
+        cart: resolve(__dirname, "wdd330-w2-bonus/src/cart/index.html"),
+        checkout: resolve(__dirname, "wdd330-w2-bonus/src/checkout/index.html"),
+        checkedout: resolve(__dirname, "wdd330-w2-bonus/src/checkout/success.html"),
+        product_pages: resolve(__dirname, "wdd330-w2-bonus/src/product_pages/index.html"),
+        product_listing: resolve(__dirname, "wdd330-w2-bonus/src/product_listing/index.html")
         /*main: resolve(__dirname, "src/js/main.js"),
         cart: resolve(__dirname, "src/js/cart.js"),
         checkout: resolve(__dirname, "src/js/checkout.js"),
